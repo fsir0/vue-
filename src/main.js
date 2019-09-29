867ee3ef6600a55bcb91a5b1135fc1fd1e075f14
fuhuang <fuhuang@1218.com.cn>
2019-09-29 10:18:53 +0800

    update:update old init vue-project

--- src/main.js
+++ src/main.js
@@ -1,34 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
-// import router from './router'
+import App from './App'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import store from './store/index'
+import router from './router'
 
 Vue.config.productionTip = false
-
-// 路由
-import Router from 'vue-router';
-import Home from './views/Home';
-import About from './views/About';
-
-Vue.use(Router)
-
-const myRouter = new Router({
-  routes:[
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/about',
-      component: About
-    }
-  ],
-  // 加上mode: 'history'之后路由上就没有了根路由上的#
-  mode: 'history'
-});
-
+// 此处引入axios仅为在组建内部也可以使用axios（通常不建议在组建内部直接axios，请求请走api）
+Vue.use(VueAxios, axios)
 new Vue({
-  // router,
-  router: myRouter,
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
 
