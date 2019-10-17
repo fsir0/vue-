46c865273c7b628cbada97cce3f086f0530ebecd
clickpen <896787174@qq.com>
2019-10-17 17:24:58 +0800

    fix eslint dev dependence

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Page1 from './views/Page1.vue'
+import Home from '@/views/Home.vue'
+import Page1 from '@/views/Page1.vue'
 
 Vue.use(Router)
 
