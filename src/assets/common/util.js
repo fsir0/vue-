c700dfe068c025b64a8d27fd91ed4c75aed322f9
clickpen <896787174@qq.com>
2019-10-28 11:27:40 +0800

    feat(util.js):增加时间格式化可可视化类型，增加兼容性

--- src/assets/common/util.js
+++ src/assets/common/util.js
@@ -1,21 +1,34 @@
 export default {
     /**
      * 格式化时间
-     * @param time Number|String|TimeObj    数字、字符串类型10、13位的时间戳，或者时间对象
+     * @param time Number|String|TimeObj    数字、字符串类型10、13-17位的时间戳，或者已经格式化的时间字符串，以及时间对象字符串，或者时间对象
      * @param type String   格式：YY-mm-dd|YY-mm-dd HH:mm|YY-mm-dd HH:mm:ss
-     * @return String 返回格式化时间
+     * @return String|Number 返回格式化时间或者时间戳
      */
     formatTime(time, type) {
         if (!time) {
             return ''
         }
         let tp = typeof (time)
-        if (tp === 'string' || tp === 'number') {
-            if ((parseInt(time) + '').length === 10) {
+        // 字符串类型时间戳
+        if (tp === 'string' && (parseInt(time) + '').length === time.length) {
+            if (time.length === 10) {
                 time *= 1000
-            } else {
+            } else if (time.length >= 13 && time.length < 17) {
                 time = parseInt(time)
+            } else {
+                return 'can`t format this time: "' + time + '"'
             }
+            // 数字类型时间戳
+        } else if (tp === 'number') {
+            if ((time + '').length === 10) {
+                time *= 1000
+            } else if ((time + '').length < 13 || (time + '').length >= 17) {
+                return 'can`t format this time: "' + time + '"'
+            }
+            // 已经格式化的字符串但不能识别或者时间对象不能识别时
+        } else if (new Date(time).toString() === 'Invalid Date') {
+            return 'can`t format this time: "' + time + '"'
         }
         // eslint-disable-next-line one-var
         let _date = new Date(time),
