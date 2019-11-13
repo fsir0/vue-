2aa7d0ecef7c9ee702558e917b6a104d1951f632
clickpen <896787174@qq.com>
2019-11-13 20:26:21 +0800

    提交一下，表示诚意

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>welecome to login page!!</div>
+    <div class="log-wrapper">welecome to login page!!</div>
 </template>
 <script>
 export default {}
 </script>
-<style>
+<style lang="less">
+.log-wrapper {
+    width: 100%;
+}
 </style>
