01ad3ebc47adeda68239a3e7621efad3d97929fe
clickpen <896787174@qq.com>
2020-06-12 18:31:23 +0800

    fix(cityList):增加城市列表假数据

--- src/components/changeCity/cityList/cityList.vue
+++ src/components/changeCity/cityList/cityList.vue
@@ -4,10 +4,10 @@
         <div class="city-firstName-letters-wrapper">
             <a v-for="ele in cityDataList" :key="ele[0]" :href="'#anchorTo' + ele[0]">{{ele[0]}}</a>
         </div>
-        <dl v-for="ele in cityDataList" :key="ele[0]">
-            <dt :id="'anchorTo' + ele[0]">{{ele[0]}}</dt>
-            <dd>
-                <a v-for="el in ele[1]" :key="el.id" href="/index">{{el.name}}</a>
+        <dl class="city-list-card" v-for="ele in cityDataList" :key="ele[0]" :id="'anchorTo' + ele[0]">
+            <dt class="city-list-letter-title">{{ele[0]}}</dt>
+            <dd class="city-list-letters-card">
+                <a v-for="el in ele[1]" :key="el.id" :class="{'advice': (el.rank === 'S' || el.rank === 'A')}" href="/index">{{el.name}}</a>
             </dd>
         </dl>
     </div>
